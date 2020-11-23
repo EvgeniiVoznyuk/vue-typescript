cd8db32aa25ba60c8e6d71c05f6798ab9fa76af2
Evgenii <voznukzhenia@gmail.com>
2020-11-23 13:34:56 +0200

    navbar

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+/* eslint-disable */
+// @ts-ignore
+import createServer from '@/mirage/server';
 import Vue from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+createServer();
 
 Vue.config.productionTip = false;
 
