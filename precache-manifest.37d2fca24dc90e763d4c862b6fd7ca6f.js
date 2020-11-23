c89a230e063be137085869e61e4f31fd4969784b
Evgenii <voznukzhenia@gmail.com>
2020-11-23 13:41:01 +0200

    gh-pages

---
+++ precache-manifest.37d2fca24dc90e763d4c862b6fd7ca6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374599a4a67e438c15a4",
+    "url": "/vue-type/css/app.ca936927.css"
+  },
+  {
+    "revision": "06be9b27f605650776b9",
+    "url": "/vue-type/css/chunk-vendors.2d065fb2.css"
+  },
+  {
+    "revision": "ffc616a3b559dfb85131e6beeef818f6",
+    "url": "/vue-type/index.html"
+  },
+  {
+    "revision": "374599a4a67e438c15a4",
+    "url": "/vue-type/js/app.7ba5c722.js"
+  },
+  {
+    "revision": "06be9b27f605650776b9",
+    "url": "/vue-type/js/chunk-vendors.0b2a1db0.js"
+  },
+  {
+    "revision": "bc9d6293da408e5ba574e57efa3cd35a",
+    "url": "/vue-type/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-type/robots.txt"
+  }
+]);
